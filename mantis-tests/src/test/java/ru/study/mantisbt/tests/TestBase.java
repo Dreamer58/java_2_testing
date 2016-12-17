cf30ba3cce056f8bb6ed3ffd48b9ee2a51b09011
Dreamer58 <shilov_work@mail.ru>
2016-12-17 17:14:53 +0400

    add new module to tests for MantisBT

---
+++ mantis-tests/src/test/java/ru/study/mantisbt/tests/TestBase.java
@@ -0,0 +1,26 @@
+package ru.study.mantisbt.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.study.mantisbt.appmanager.ApplicationManager;
+
+
+/**
+ * Created by Dreamer on 27.10.2016.
+ */
+public class TestBase {
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+
+}
