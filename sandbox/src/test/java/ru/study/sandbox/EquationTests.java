91f392c6a7afc1739969b69eb6039b491ab07e00
Dreamer58 <shilov_work@mail.ru>
2016-12-23 23:31:45 +0400

    return good test

--- sandbox/src/test/java/ru/study/sandbox/EquationTests.java
+++ sandbox/src/test/java/ru/study/sandbox/EquationTests.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 public class EquationTests {
     @Test
     public void tets0(){
-        Equation e = new Equation(1, 1, 0);
+        Equation e = new Equation(1, 1, 1);
         Assert.assertEquals(e.rootNumber(), 0);
     }
 
